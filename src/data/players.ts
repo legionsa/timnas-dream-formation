3502c57821b76da8a5bc5e4b444d5223d013a56e
legionsa <dskalvinlim@gmail.com>
2024-11-19 13:18:24 +0000

    yukkk

--- src/data/players.ts
+++ src/data/players.ts
@@ -4,217 +4,217 @@ export const players: Player[] = [
   {
     id: 0,
     name: "Maarten Paes",
-    image: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?w=150&h=150&fit=crop"
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Maarten_Paes_2024.png/800px-Maarten_Paes_2024.png"
   },
   {
     id: 1,
     name: "Nadeo Argawinata",
-    image: "https://images.unsplash.com/photo-1544699652-70c2deb74861?w=150&h=150&fit=crop"
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Nadeo_Argawinata.jpg/330px-Nadeo_Argawinata.jpg"
   },
   {
     id: 2,
     name: "Ernando Ari",
-    image: "https://images.unsplash.com/photo-1564164841584-391b5139b8b6?w=150&h=150&fit=crop"
+    image: "https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/p2/80/2024/04/21/ernando-u23-882255412.jpeg"
   },
   {
     id: 3,
     name: "Muhammad Riyandi",
-    image: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?w=150&h=150&fit=crop"
+    image: "https://www.pssi.org/files/uploads/player/photo_action/2024/Mar/27/6603c6ce84655/riyandi-320x_.jpg?token=02e4c964fa8964ef5400cf9df420549a"
   },
   {
     id: 4,
     name: "Adi Satrio",
-    image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=150&h=150&fit=crop"
+    image: "https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/p3/53/2024/05/17/muhammad-adi-satryo-kiper-temnas-1967602289.jpg"
   },
   {
     id: 5,
     name: "Syahrul Fadil",
-    image: "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?w=150&h=150&fit=crop"
+    image: "https://assets.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2022/04/05/956926933.jpg"
   },
   {
     id: 6,
     name: "Kevin Diks",
-    image: "https://images.unsplash.com/photo-1566492031773-4f4e44671857?w=150&h=150&fit=crop"
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdvpqe6T4SbfSPdfrtQahd2fgxTGHzzPh7jg&s"
   },
   {
     id: 7,
     name: "Jay Idzes",
-    image: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=150&h=150&fit=crop"
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Jay_Idzes_-_Go_Ahead_Eagles_-_FC_Volendam_-_52915157035_%28cropped%2C_2023%29.jpg/800px-Jay_Idzes_-_Go_Ahead_Eagles_-_FC_Volendam_-_52915157035_%28cropped%2C_2023%29.jpg"
   },
   {
     id: 8,
     name: "Jordi Amat",
-    image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=150&h=150&fit=crop"
+    image: "https://cdn.rri.co.id/berita/Pusat_Pemberitaan/o/1728907771824-GZ2KgpXbsAAAmR8/008kpc1tt0yazha.jpeg"
   },
   {
     id: 9,
     name: "Rizky Ridho",
-    image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=150&h=150&fit=crop"
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Indonesia-Brunei_17_October_2023_%28Players%29_23.jpg/330px-Indonesia-Brunei_17_October_2023_%28Players%29_23.jpg"
   },
   {
     id: 10,
     name: "Sandy Walsh",
-    image: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=150&h=150&fit=crop"
+    image: "https://cdn0-production-images-kly.akamaized.net/n7XsuDFMNSM79mdDUHUXJ69b1uY=/0x0:0x0/1200x1200/filters:quality(75):strip_icc():format(webp):watermark(kly-media-production/assets/images/watermarks/bola/watermark-color-square-new.png,1125,20,0)/kly-media-production/medias/4608871/original/013095100_1697127268-20231012IQ_Timnas_Indonesia_Vs_Brunei_Darussalam_12.JPG"
   },
   {
     id: 11,
     name: "Pratama Arhan",
-    image: "https://images.unsplash.com/photo-1507038732509-8b1a9623223a?w=150&h=150&fit=crop"
+    image: "https://cdn1-production-images-kly.akamaized.net/kG6zMWYF5al5eQUP5CFcbdg0OfA=/0x0:0x0/1200x1200/filters:quality(75):strip_icc():format(webp):watermark(kly-media-production/assets/images/watermarks/bola/watermark-color-square-new.png,1125,20,0)/kly-media-production/medias/4426403/original/009268600_1683974162-033A9039.JPG"
   },
   {
     id: 12,
     name: "Muhammad Ferarri",
-    image: "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?w=150&h=150&fit=crop"
+    image: "https://img.okezone.com/content/2023/03/08/51/2777220/alihkan-fokus-ke-piala-dunia-u-20-2023-muhammad-ferarri-soroti-masalah-ini-di-timnas-indonesia-u-20-bFWTU8MS58.jpg"
   },
   {
     id: 13,
     name: "Calvin Verdonk",
-    image: "https://images.unsplash.com/photo-1541647376583-8934aaf3448a?w=150&h=150&fit=crop"
+    image: "https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/p2/01/2024/11/16/Calvin-Verdonk-Dery-RidwansahJPG-4183443785.jpg"
   },
   {
     id: 14,
     name: "Shayne Pattynama",
-    image: "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?w=150&h=150&fit=crop"
+    image: "https://cdn0-production-images-kly.akamaized.net/7PphF97SymMHhSOXZ3Ap1QS3Evc=/696x0:2384x1688/1200x1200/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/4608870/original/071867300_1697127242-20231012IQ_Timnas_Indonesia_Vs_Brunei_Darussalam_11.JPG"
   },
   {
     id: 15,
     name: "Justin Hubner",
-    image: "https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?w=150&h=150&fit=crop"
+    image: "https://kilasjatim.com/wp-content/uploads/2024/07/Justin-Hubner-buka-suara-soal-debut-hingga-singgung-soal-formasi-112440756.webp"
   },
   {
     id: 16,
     name: "Yance Sayuri",
-    image: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?w=150&h=150&fit=crop"
+    image: "https://imgsrv2.voi.id/QYrzZGnpmemmh7IEWUz_koZlrYraJJcUDsjjuE-t_JE/auto/1280/853/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8zNDExMDgvMjAyMzEyMjExMTE3LW1haW4uY3JvcHBlZF8xNzAzMTMyMjc1LmpwZw.jpg"
   },
   {
     id: 17,
     name: "Mees Hilgers",
-    image: "https://images.unsplash.com/photo-1563237023-b1e970526dcb?w=150&h=150&fit=crop"
+    image: "https://asset-2.tstatic.net/wow/foto/bank/images/Tambahan-Amunisi-Terbaru-Timnas-Indonesia-Mees-Hilgers.jpg"
   },
   {
     id: 18,
     name: "Asnawi Mangkualam",
-    image: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?w=150&h=150&fit=crop"
+    image: "https://cdn0-production-images-kly.akamaized.net/h53LbFl9tzXS_ynZqnmFvsI2MuE=/800x800/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3859475/original/079782300_1640846896-269682217_437882561377432_5894104055758407690_n.jpg"
   },
   {
     id: 19,
     name: "Wahyu Prasetyo",
-    image: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=150&h=150&fit=crop"
+    image: "https://pict.sindonews.net/dyn/850/pena/news/2023/11/09/11/1247217/siapakah-wahyu-prasetyo-si-hulk-yang-dipanggil-timnas-indonesia-rtn.jpeg"
   },
   {
     id: 20,
     name: "Edo Febriansah",
-    image: "https://images.unsplash.com/photo-1507038732509-8b1a9623223a?w=150&h=150&fit=crop"
+    image: "https://img.okezone.com/content/2023/05/09/49/2810719/profil-edo-febriansyah-bek-timnas-indonesia-pilihan-shin-tae-yong-yang-sukses-taklukkan-hati-luis-milla-WMGovClU6a.jpg"
   },
   {
     id: 21,
     name: "Elkan Baggott",
-    image: "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?w=150&h=150&fit=crop"
+    image: "https://asset-2.tstatic.net/tribunnews/foto/bank/images/timnas-u23-indonesia-menang-telak-9-0_20230909_225111.jpg"
   },
   {
     id: 22,
     name: "Marselino Ferdinan",
-    image: "https://images.unsplash.com/photo-1541647376583-8934aaf3448a?w=150&h=150&fit=crop"
+    image: "https://upload.wikimedia.org/wikipedia/commons/0/02/Marselino_Ferdinan.png"
   },
   {
     id: 23,
     name: "Witan Sulaeman",
-    image: "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?w=150&h=150&fit=crop"
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIUrhdbbT7zTtVc85yQOqAAxTEGHKSgXd8jQ&s"
   },
   {
     id: 24,
     name: "	Egy Maulana Vikri",
-    image: "https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?w=150&h=150&fit=crop"
+    image: "https://kiblatriau.com/assets/berita/original/68969493685-egi_maulana_ok.jpg"
   },
   {
     id: 25,
     name: "Yakob Sayuri",
-    image: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?w=150&h=150&fit=crop"
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Yakob_Sayuri.png/330px-Yakob_Sayuri.png"
   },
   {
     id: 26,
     name: "Ricky Kambuaya",
-    image: "https://images.unsplash.com/photo-1563237023-b1e970526dcb?w=150&h=150&fit=crop"
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Indonesia-Brunei_17_October_2023_%28Players%29_49.jpg/330px-Indonesia-Brunei_17_October_2023_%28Players%29_49.jpg"
   },
   {
     id: 27,
     name: "Thom Haye",
-    image: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?w=150&h=150&fit=crop"
+    image: "https://cdn.rri.co.id/berita/Bengkulu/o/1726315995804-66b376a259c15-gelandang-timnas-indonesia-thom-haye_1265_711/5t2jc7sctp13ubx.jpeg"
   },
   {
     id: 28,
     name: "Nathan Tjoe-A-On",
-    image: "https://images.unsplash.com/photo-1544699652-70c2deb74861?w=150&h=150&fit=crop"
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Nathan_Tjoe-A-On.jpg/330px-Nathan_Tjoe-A-On.jpg"
   },
   {
     id: 29,
     name: "Ivar Jenner",
-    image: "https://images.unsplash.com/photo-1564164841584-391b5139b8b6?w=150&h=150&fit=crop"
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Ivar_Jenner_2024.jpg/330px-Ivar_Jenner_2024.jpg"
   },
   {
     id: 30,
     name: "Eliano Reijnders",
-    image: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?w=150&h=150&fit=crop"
+    image: "https://media.suara.com/pictures/1600x840/2024/10/01/29333-eliano-reijnders-pakai-nomor-punggu-7-attimnasindonesia.jpg"
   },
   {
     id: 31,
     name: "Rachmat Irianto",
-    image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=150&h=150&fit=crop"
+    image: "https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/2023/01/08/20230108_114019jpg-20230108011741.jpg"
   },
   {
     id: 32,
     name: "Arkhan Fikri",
-    image: "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?w=150&h=150&fit=crop"
+    image: "https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/riaupos/1521-asdfvgda.jpg"
   },
   {
     id: 33,
     name: "Marc Klok",
-    image: "https://images.unsplash.com/photo-1566492031773-4f4e44671857?w=150&h=150&fit=crop"
+    image: "https://tmssl.akamaized.net//images/foto/galerie/marc-klok-1672366434-99292.jpg?lm=1672366449"
   },
   {
     id: 34,
     name: "Adam Alis",
-    image: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=150&h=150&fit=crop"
+    image: "https://img.a.transfermarkt.technology/portrait/big/505890-1688189974.jpg?lm=1"
   },
   {
     id: 35,
     name: "Saddil Ramdani",
-    image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=150&h=150&fit=crop"
+    image: "https://asset-2.tstatic.net/tribunnews/foto/bank/images/saddil-ramdani-21121-1.jpg"
   },
   {
     id: 36,
     name: "	Rafael Struick",
-    image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=150&h=150&fit=crop"
+    image: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Rafael_Struick_Indonesia.jpg"
   },
   {
     id: 37,
     name: "Ragnar Oratmangoen",
-    image: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=150&h=150&fit=crop"
+    image: "https://media.suara.com/pictures/653x366/2024/06/02/79841-timnas-indonesia-ragnar-oratmangoen.jpg"
   },
   {
     id: 38,
     name: "Hokky Caraka",
-    image: "https://images.unsplash.com/photo-1507038732509-8b1a9623223a?w=150&h=150&fit=crop"
+    image: "https://upload.wikimedia.org/wikipedia/commons/4/4f/Indonesia-Brunei_17_October_2023_%28Players%29_52.jpg"
   },
   {
     id: 39,
     name: "Ramadhan Sananta",
-    image: "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?w=150&h=150&fit=crop"
+    image: "https://www.blibli.com/friends-backend/wp-content/uploads/2024/05/B140041-cover-profil-ramadhan-sananta-scaled.jpg"
   },
   {
     id: 40,
     name: "Dimas Drajad",
-    image: "https://images.unsplash.com/photo-1541647376583-8934aaf3448a?w=150&h=150&fit=crop"
+    image: "https://www.pssi.org/files/uploads/player/photo_action/2024/Jun/14/666c0d1bd355f/dimas-drajad-320x_.jpg?token=3bcccbc86598db42e8d8fd79590ff4ba"
   },
   {
     id: 41,
     name: "Malik Risaldi",
-    image: "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?w=150&h=150&fit=crop"
+    image: "https://awsimages.detik.net.id/community/media/visual/2024/10/24/malik-risaldi_169.jpeg?w=1200"
   },
   {
     id: 42,
     name: "Dendy Sulistyawan",
-    image: "https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?w=150&h=150&fit=crop"
+    image: "https://www.pssi.org/files/uploads/player/photo_action/2023/Sep/19/6509991ebcc73/ds-vs-turkmenistan-png-320x_.png?token=092f45f978899f6aec40b681859d41be"
   }
 ];
 
